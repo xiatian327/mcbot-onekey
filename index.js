eeb1084303933e018dbe678f368677c8843b9432
xiatian327 <136238840+xiatian327@users.noreply.github.com>
2026-05-30 12:01:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 
 const servers = [
   {
-    host: '15.235.144.77',
-    port: 11388,
+    host: 'frostwolfdefenders675.mcsh.io',
+    port: 25565,
     // username: 'AFK_Bot',   // 可选，不写则随机
     // version: '1.21.1'     // 可选，不写则自动检测
   }
